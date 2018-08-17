a7671ec19786858c97fd84489e26271a0fd5c11c
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-08-17 17:21:33 +0200

    add ADR, because we can lose the connection in a certain case

--- e3-cmds/e3-gen5kWPS.cmd
+++ e3-cmds/e3-gen5kWPS.cmd
@@ -1,46 +1,24 @@
+
 require stream,2.7.11
 require iocStats,1856ef5
-
 
 epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
 epicsEnvSet("IOC", "iocgenesysGEN5kWPS")
 
-epicsEnvSet("DB_TOP",      "$(TOP)/genesysGEN5kWPSApp/Db")
-epicsEnvSet("ENV_CMD_TOP", "$(TOP)/iocBoot/$(IOC)")
+epicsEnvSet("DB_TOP", "$(TOP)/genesysGEN5kWPSApp/Db")
 
 epicsEnvSet("STREAM_PROTOCOL_PATH", ".:$(DB_TOP)")
+
+epicsEnvSet("TS2MOXA01", "10.0.4.91")
+
+
+iocshLoad "$(E3_CMD_TOP)/iocStats.cmd" "IOC=$(IOC):IocStats"
 
 epicsEnvSet(P, GEM5kWPS)
 epicsEnvSet(R, RFLab)
 
-epicsEnvSet("IOCST", "$(P)-$(R):IocStats")
-dbLoadRecords("iocAdminSoft.db",  "IOC=${IOCST}")
 
-
-< $(ENV_CMD_TOP)/GEN300-17-env.cmd
-
-epicsEnvSet("TS2MOXA01", "172.16.110.20")
-
-epicsEnvSet("PORT1", "4001")
-drvAsynIPPortConfigure("$(PORT1)","$(TS2MOXA01):$(PORT1)",0,0,0)
-asynOctetSetInputEos($(PORT1),  0, "\r")
-asynOctetSetOutputEos($(PORT1), 0, "\r")
-dbLoadRecords("$(DB_TOP)/genesysGEN5kWPS.db", "P=$(P)-$(R):GEN5kWPS:,PORT=$(PORT1),OVPMIN=$(OVPMIN),OVPMAX=$(OVPMAX),UVLMIN=$(UVLMIN),UVLMAX=$(UVLMAX),VMIN=$(VMIN),VMAX=$(VMAX),CMIN=$(CMIN),CMAX=$(CMAX)")
-
-
-epicsEnvSet("PORT2", "4002")
-drvAsynIPPortConfigure("$(PORT2)","$(TS2MOXA01):$(PORT2)",0,0,0)
-asynOctetSetInputEos($(PORT2),  0, "\r")
-asynOctetSetOutputEos($(PORT2), 0, "\r")
-dbLoadRecords("$(DB_TOP)/genesysGEN5kWPS.db", "P=$(P)-$(R):GEN5kWPS:,PORT=$(PORT2),OVPMIN=$(OVPMIN),OVPMAX=$(OVPMAX),UVLMIN=$(UVLMIN),UVLMAX=$(UVLMAX),VMIN=$(VMIN),VMAX=$(VMAX),CMIN=$(CMIN),CMAX=$(CMAX)")
-
-
-epicsEnvSet("PORT3", "4003")
-drvAsynIPPortConfigure("$(PORT3)","$(TS2MOXA01):$(PORT3)",0,0,0)
-asynOctetSetInputEos($(PORT3),  0, "\r")
-asynOctetSetOutputEos($(PORT3), 0, "\r")
-dbLoadRecords("$(DB_TOP)/genesysGEN5kWPS.db", "P=$(P)-$(R):GEN5kWPS:,PORT=$(PORT3),OVPMIN=$(OVPMIN),OVPMAX=$(OVPMAX),UVLMIN=$(UVLMIN),UVLMAX=$(UVLMAX),VMIN=$(VMIN),VMAX=$(VMAX),CMIN=$(CMIN),CMAX=$(CMAX)")
-
+iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSTOP:,DB_TOP=$(DB_TOP)"
 
 iocInit
 
--- e3-cmds/gen5kWPS_n.cmd
+++ e3-cmds/gen5kWPS_n.cmd
@@ -1,4 +1,5 @@
-# iocshLoad "$(TOP)/gen5kWPS_n.cmd" "MOXA_IP=10.0.0.1,MOXA_PORT=4001,PREFIX=$(P)-$(R):GEN5kWPS:,DB_TOP=$(DB_TOP)"
+
+#iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSTOP:,DB_TOP=$(DB_TOP)"
 
 
 # Table 7-3 : Voltage Programming Range
@@ -17,10 +18,11 @@ epicsEnvSet("_OVPMAX", "330")
 epicsEnvSet("_UVLMIN", "0")
 epicsEnvSet("_UVLMAX", "285")
 
+
 epicsEnvSet("PORT$(MOXA_PORT)", "$(MOXA_PORT)")
 drvAsynIPPortConfigure("PORT$(MOXA_PORT)","$(MOXA_IP):$(MOXA_PORT)",0,0,0)
 asynOctetSetInputEos(PORT$(MOXA_PORT),  0, "\r")
 asynOctetSetOutputEos(PORT$(MOXA_PORT), 0, "\r")
-dbLoadRecords("$(DB_TOP)/genesysGEN5kWPS.db", "P=$(PREFIX),PORT=PORT$(MOXA_PORT),OVPMIN=$(_OVPMIN),OVPMAX=$(_OVPMAX),UVLMIN=$(_UVLMIN),UVLMAX=$(_UVLMAX),VMIN=$(_VMIN),VMAX=$(_VMAX),CMIN=$(_CMIN),CMAX=$(_CMAX)")
+dbLoadRecords("$(DB_TOP)/genesysGEN5kWPS.db", "P=$(PREFIX),PORT=PORT$(MOXA_PORT),ADDR=$(ADDR),OVPMIN=$(_OVPMIN),OVPMAX=$(_OVPMAX),UVLMIN=$(_UVLMIN),UVLMAX=$(_UVLMAX),VMIN=$(_VMIN),VMAX=$(_VMAX),CMIN=$(_CMIN),CMAX=$(_CMAX)")
 
 
