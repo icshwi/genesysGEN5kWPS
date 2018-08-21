f529b8f43dd8a3cf48bf613cc4a0520ec2169eec
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-08-21 11:31:35 +0200

    update pv according to naming

--- e3-cmds/e3-gen5kWPS.cmd
+++ e3-cmds/e3-gen5kWPS.cmd
@@ -11,14 +11,16 @@ epicsEnvSet("STREAM_PROTOCOL_PATH", ".:$(DB_TOP)")
 
 epicsEnvSet("TS2MOXA01", "10.0.4.91")
 
-
-iocshLoad "$(E3_CMD_TOP)/iocStats.cmd" "IOC=$(IOC):IocStats"
-
-epicsEnvSet(P, GEM5kWPS)
-epicsEnvSet(R, RFLab)
+epicsEnvSet(P, TS2-010RFC:)
+epicsEnvSet(R, RFS-SOLPS-001:)
 
 
-iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSTOP:,DB_TOP=$(DB_TOP)"
+iocshLoad "$(E3_CMD_TOP)/iocStats.cmd" "IOC=$(P)$(R)IocStats"
+
+
+
+
+iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)$(R),DB_TOP=$(DB_TOP)"
 
 iocInit
 
--- e3-cmds/gen5kWPSs.cmd
+++ e3-cmds/gen5kWPSs.cmd
@@ -3,7 +3,6 @@ require stream,2.7.11
 require iocStats,1856ef5
 
 epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
-epicsEnvSet("IOC", "iocgenesysGEN5kWPS")
 
 epicsEnvSet("DB_TOP", "$(TOP)/genesysGEN5kWPSApp/Db")
 
@@ -11,15 +10,19 @@ epicsEnvSet("STREAM_PROTOCOL_PATH", ".:$(DB_TOP)")
 
 epicsEnvSet("TS2MOXA01", "172.16.110.20")
 
-iocshLoad "$(E3_CMD_TOP)/iocStats.cmd" "IOC=$(IOC):IocStats"
+epicsEnvSet(P, TS2-010RFC:)
+epicsEnvSet(R, RFS-SOLPS)
+epicsEnvSet(R1, $(R)-001:)
+epicsEnvSet(R2, $(R)-002:)
+epicsEnvSet(R3, $(R)-003:)
+epicsEnvSet(IOC, $(P)$(R))
+
+iocshLoad "$(E3_CMD_TOP)/iocStats.cmd" "IOC=$(IOC)-IocStats"
 
 
-epicsEnvSet(P, TS2)
-epicsEnvSet(R, RFLPS)
-
-iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSTOP:,DB_TOP=$(DB_TOP)"
-iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4002,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSMID:,DB_TOP=$(DB_TOP)"
-iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4003,ADDR=6,PREFIX=$(P)-$(R):GEN5kWPSBOT:,DB_TOP=$(DB_TOP)"
+iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4001,ADDR=6,PREFIX=$(P)$(R1),DB_TOP=$(DB_TOP)"
+iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4002,ADDR=6,PREFIX=$(P)$(R2),DB_TOP=$(DB_TOP)"
+iocshLoad "$(E3_CMD_TOP)/gen5kWPS_n.cmd" "MOXA_IP=$(TS2MOXA01),MOXA_PORT=4003,ADDR=6,PREFIX=$(P)$(R3),DB_TOP=$(DB_TOP)"
 
 iocInit
 
